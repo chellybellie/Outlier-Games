cea0ba578aaafbf86523331357cc8f9d576f9d4b
gingerfarrar <ginger.farrar@students.aie.edu.au>
2018-01-19 09:50:35 -0800

    bs

---
+++ Assets/ginger/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausemenu;  
+
+	public void Pause()
+    {
+        pausemenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pausemenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
