ad419b30ac976bfdcf8627d80a562976e90839c6
Sergei James Lyle <james.lyle@students.aie.edu.au>
2018-01-19 12:59:48 -0800

    added ai

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        var x = Input.GetAxis("horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Rotate(0, 0, z);
+	}
+}
