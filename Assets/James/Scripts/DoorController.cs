0b04f51681a93f283e0caee752d0ed48752193e9
Sergei James Lyle <james.lyle@students.aie.edu.au>
2018-04-04 16:30:13 -0700

    door animation complete

--- Assets/James/Scripts/DoorController.cs
+++ Assets/James/Scripts/DoorController.cs
@@ -5,22 +5,14 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     public GameObject Door;
-    public bool doorIsOpening;
+    public bool Open;
 
     // Update is called once per frame
     void Update()
     {
-        if (doorIsOpening == true)
-        {
-            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
-        }
-        if (Door.transform.position.y > 2.5f)
-        {
-            doorIsOpening = false;
-        }
         if (Input.GetKeyDown(KeyCode.F))
         {
-            doorIsOpening = true;
+            Open = true;
         }
     }
 }
